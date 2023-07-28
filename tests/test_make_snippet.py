51afe861dad00d5a993c28ce70ca21c29aba3bee
sarahellis <sarahellislondon@gmail.com>
2023-07-28 09:29:54 +0100

    Change of function and file name

--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -1,12 +1,12 @@
 import pytest
-from lib.personal_diary_system import *
+from lib.make_snippet import *
 
 '''
 return first five words of given string
 '''
 
-def test_diary_returns_first_five_words_of_string():
-    result = personal_diary_system("return first five words is")
+def test_snippet_returns_first_five_words_of_string():
+    result = make_snippet("return first five words is")
     assert result == "return first five words is"
 
 '''
@@ -14,23 +14,23 @@ if string is longer than 5 words
 return first 5 words followed by ... instead of rest of string
 '''
 
-def test_diary_returns_more_than_five_words_of_string():
-    result = personal_diary_system("return first five words is next step")
+def test_snippet_returns_more_than_five_words_of_string():
+    result = make_snippet("return first five words is next step")
     assert result == "return first five words is..."
 '''
 if string is empty
 return just the string
 '''
-def test_diary_returns_empty_string():
-    result = personal_diary_system("")
+def test_snippet_returns_empty_string():
+    result = make_snippet("")
     assert result == ""
 '''
 if argument is an integer
 return error
 '''
-def test_diary_returns_error_for_non_string():
-    # personal_diary_system(5)
+def test_snippet_returns_error_for_non_string():
+    # make_snippet(5)
     with pytest.raises(Exception) as e:
-        result = personal_diary_system(5)
+        result = make_snippet(5)
     error_message = str(e.value)
     assert error_message == "Please enter a string"
